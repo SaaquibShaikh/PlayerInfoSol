edf6c00474f641f1912a4559e3912d993ee943e7
SaaquibShaikh <saaquibs25@gmail.com>
2022-01-03 22:54:28 +0530

    added helper

---
+++ PlayerInfoMS/DataBaseAccess/ConnStringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerInfoMS.DataBaseAccess
+{
+    public class ConnStringHelper
+    {
+        public static string getConnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
